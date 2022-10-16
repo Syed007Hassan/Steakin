0574c8a12184d416dae344d763028176979d5844
Syed007Hassan <k201052@nu.edu.pk>
2022-10-16 21:04:40 +0500

    changed database.js

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'fast',
+    password: 'fast123',
     database: 'restaurantdb'
 });
 
