314786b8b05e71c80e7134f44ac7b3568998a2e2
Syed007Hassan <k201052@nu.edu.pk>
2022-11-15 07:22:30 +0500

    Changes

--- app.test.js
+++ app.test.js
@@ -32,16 +32,16 @@ describe("Testing the connectivity of MySQL", () => {
   });
 
 
-// describe("Test the root path", () => {
-//     test("It should response the GET method", done => {
-//       request(app)
-//         .get("/")
-//         .then(response => {
-//           expect(response.statusCode).toBe(200);
-//           done();
-//         });
-//     });
-//   });
+describe("Test the root path", () => {
+    test("It should response the GET method", done => {
+      request(app)
+        .get("/")
+        .then(response => {
+          expect(response.statusCode).toBe(200);
+          done();
+        });
+    });
+  });
 
 // describe("Test the root path", () => {
 //     test("It should response the GET method", async () => {
