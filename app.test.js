b10531c47432eb3a9f58a2311953bd03f3790596
Syed007Hassan <k201052@nu.edu.pk>
2022-12-06 01:14:33 +0500

    commented

--- app.test.js
+++ app.test.js
@@ -11,67 +11,67 @@ import { response } from "express";
 // const app = require('./app.js');
 // const bodyParser = require('body-parser');
 
-jest.useFakeTimers();
+// jest.useFakeTimers();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json());
-jest.setTimeout(30000);
+// app.use(bodyParser.json());
+// jest.setTimeout(30000);
 
-const dbtest = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "fast123",
-  database: "restaurantdb",
-});
+// const dbtest = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "fast123",
+//   database: "restaurantdb",
+// });
 
-let currentDate = new Date().toJSON().slice(0, 10);
- // console.log(currentDate);
+// let currentDate = new Date().toJSON().slice(0, 10);
+//  // console.log(currentDate);
 
-  var sql = `INSERT INTO contactus (FirstName,LastName,Email,Phone,Message,date)
-   VALUES ("fname", "lname", "email", "phone", "message", "${currentDate}")`;
+//   var sql = `INSERT INTO contactus (FirstName,LastName,Email,Phone,Message,date)
+//    VALUES ("fname", "lname", "email", "phone", "message", "${currentDate}")`;
 
- const testresult =  con.query(sql, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
+//  const testresult =  con.query(sql, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
  
-      return response.statusCode;
-    }
-  });
+//       return response.statusCode;
+//     }
+//   });
 
-describe("Testing the connectivity of MySQL", () => {
-  test("Connection setup using database.js file", async () => {
-    const response = con.connect();
-    const response1 = dbtest.connect();
-    expect(response).toBe(response1);
-  });
-});
+// describe("Testing the connectivity of MySQL", () => {
+//   test("Connection setup using database.js file", async () => {
+//     const response = con.connect();
+//     const response1 = dbtest.connect();
+//     expect(response).toBe(response1);
+//   });
+// });
 
-describe("Test the root path", () => {
-  test("It should response the GET method", (done) => {
-    request(app)
-      .get("/")
-      .then((response) => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
+// describe("Test the root path", () => {
+//   test("It should response the GET method", (done) => {
+//     request(app)
+//       .get("/")
+//       .then((response) => {
+//         expect(response.statusCode).toBe(200);
+//         done();
+//       });
+//   });
 
-});
+// });
 
-describe("Test the post request", () => {
+// describe("Test the post request", () => {
 
-  test("should respond with a 200 status code", (done) => {
-    const response = request(app).post("/page-contacts").send({
-      fname: "ali",
-      lname: "password",
-      email: "asd@gmail.com" ,
-      phone: "234234", 
-      message: "hello world" 
-    });
-    expect(response.statusCode).toBe(testresult);
-    done();
-  })
+//   test("should respond with a 200 status code", (done) => {
+//     const response = request(app).post("/page-contacts").send({
+//       fname: "ali",
+//       lname: "password",
+//       email: "asd@gmail.com" ,
+//       phone: "234234", 
+//       message: "hello world" 
+//     });
+//     expect(response.statusCode).toBe(testresult);
+//     done();
+//   })
 
-  });
+//   });
