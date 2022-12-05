bb5a21f461b7f3d76a30061c202282791f1f61f0
Syed007Hassan <k201052@nu.edu.pk>
2022-12-06 01:13:42 +0500

    changes in database

--- database.js
+++ database.js
@@ -2,10 +2,10 @@
 import mysql from 'mysql2';
 
 const con = mysql.createConnection({
-    host: 'localhost',
+    // host: 'localhost',
     user: 'root',
-    password: 'fast123',
-    database: 'restaurantdb'
+    password: 'QOXlvy89771',
+    database: 'node419078-stakin-restaurant.j.layershift.co.uk/restaurantdb'
 });
 
 
